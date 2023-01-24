7ac2c5437fe7c0803eb9c611615e176efd7c3346
Manzar <manzar.hussain@telus.com>
2023-01-24 16:38:18 -0500

    Exercise 7: Validations for both models completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter the store name:"
+name = gets.chomp
+puts "Enter the store annual_revenue:"
+annual_revenue = gets.chomp.to_i
+puts "Enter the store mens_apparel(true/false):"
+mens_apparel = gets.chomp
+puts "Enter the store womens_apparel(true/false):"
+womens_apparel = gets.chomp
+store = Store.new(name: name, annual_revenue: annual_revenue, mens_apparel: mens_apparel, womens_apparel: womens_apparel)
+
+if store.save
+  puts "Store created successfully!"
+else
+  puts "Error creating store:"
+  store.errors.full_messages.each { |msg| puts msg }
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :at_least_one_apparel
+ 
+  def at_least_one_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "The store must carry at least one of the men's or women's apparel")
+    end
+  end
 end
